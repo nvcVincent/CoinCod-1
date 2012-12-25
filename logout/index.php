1623206e3fc7f881fbae923a2a06100a5803b199
Wilson <wilson@nexvend.com>
2012-12-26 00:38:48 +0800

    update logout page

--- logout/index.php
+++ logout/index.php
@@ -1,23 +1,7 @@
 <?php
-	ob_start();
 	include "../config.php";
+	session_start();
+	session_destroy();
+	unset( $_SESSION );
+	header("location:$PREFIX"); 
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8" HTTP-EQUIV="Refresh" CONTENT="0;URL=<?php echo $PREFIX; ?>"/>
-	<title>Logout</title>
-</head>
-
-<body>
-	<div id="wrapper">
-	<?php
-		session_start();
-		session_destroy();
-		unset( $_SESSION );
-		
-	</div>  <!--end div wrapper-->
-	?>
-        
-</body>
-</html>
