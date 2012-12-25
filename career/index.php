189c7d5c01015cd91d8f41cc97e2f4bff8d99f97
Wilson <wilson@nexvend.com>
2012-12-26 00:25:35 +0800

    update career page

--- career/index.php
+++ career/index.php
@@ -1,31 +1,36 @@
 <?php
-	session_start();
-	include "../config.php";
-?>
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8" />
-	<title>Career</title>
-</head>
+// Load the Savant3 class file and create an instance.
+require_once '../Savant3.php';
 
-<body>
-<div id="wrapper">
-    <?php
-		include "../template/templateheader.php";
-	?>
-    <section id="content_container">
-	<h5>Careers</h5>
-    		<section class="auction_container">					
-		Sorry, currently there are no available job openings. Please check with us again soon and hopefully we can provide you one in coming few months time.
+// initialize template engine
+$tpl = new Savant3();
 
-		<img class="bottom" src="<?php echo $PREFIX; ?>/template/template_image/bottom/careers.png" alt="careers">
-	  		</section>  <!--end div auction_container-->
-	</section>  <!--end div content_container-->
-</div>  <!--wrapper-->
+// set search path for templates
+$tpl->addPath('template', '../template');
 
-<?php
-	include "../template/templatefooter.html";
-?>
-</body>
-</html>
+// Create a title.
+$template_path = "../template/";
+$resource_path = "../";
+$title = "Careers";
+$meta_description = "Welcome to CoinCod - a unique auction system built to draw everyone closer to their dream products.";
+
+$contentContainer = array(
+    array(
+        "title" => $title,
+        "content" => "Sorry, currently there are no available job openings. Please check with us again soon and hopefully we can provide you one in coming few months time.",
+		"bottom_image" =>'<img class="bottom" src="../template/template_image/bottom/careers.png" alt="careers">'
+    )
+);
+
+// Assign values to the Savant instance.
+$tpl->template_path = $template_path;
+$tpl->resource_path = $resource_path;
+$tpl->title = $title;
+$tpl->meta_description = $meta_description;
+$tpl->content_container = $contentContainer;
+
+// Display a template using the assigned values.
+$tpl->login = $tpl->fetch($template_path.'login.tpl');
+$tpl->header = $tpl->fetch($template_path.'header.tpl');
+$tpl->footer = $tpl->fetch($template_path.'footer.tpl');
+$tpl->display($template_path.'main.tpl');
